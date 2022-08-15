4750bb4415556da6196a6ed3265ed55b3e510981
Brian H. Buchholz <4773480+bhb603@users.noreply.github.com>
2022-08-15 11:31:21 -0400

    update client

--- ruby/calculator_client.rb
+++ ruby/calculator_client.rb
@@ -8,23 +8,32 @@ require 'grpc'
 require 'calculator_services_pb'
 
 def main
-  grpc_addr = ARGV.size > 1 ?  ARGV[1] : 'localhost:50051'
+  grpc_addr = ARGV.size > 1 ?  ARGV.shift : 'localhost:50051'
   stub = Grpc::Demo::Calculator::Stub.new(grpc_addr, :this_channel_is_insecure)
 
+  puts "Fibonaccis:"
   (0..10).each do |n|
     fib = stub.nth_fibonacci(Grpc::Demo::FibonacciParams.new(n: n))
-    puts "#{n}th fib=#{fib.value}"
+    print fib.value, " "
   end
+  puts
+
+  puts "Enter to continue"
+  STDIN.gets
 
   numbers = [6, 0, 3, 9]
   resp = stub.sum(Grpc::Demo::SumParams.new(numbers: numbers))
-  puts "sum of '#{numbers}'=#{resp.sum}"
+  puts "\nsum of '#{numbers}'=#{resp.sum}"
 
+  puts "Enter to continue"
+  STDIN.gets
+
+  puts "\nRandom stream:"
   random_stream_resp = stub.random_stream(Grpc::Demo::RandomStreamParams.new(min: -10, max: 10, count: 10))
   random_stream_resp.each do |random_num|
     print random_num.value, " "
   end
-  puts 'DONE'
+  puts "\nDONE"
 end
 
 
